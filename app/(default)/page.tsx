b5cdb786829462b8a2c27c8a33441a02b8fa0615
victor <victorezeabikwa@gmail.com>
2024-07-17 11:16:06 +0100

    oocf repo

---
+++ app/(default)/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'OOCF',
+  description: 'Page description',
+}
+
+import Hero from '@/components/hero'
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      
+    </>
+  )
+}
