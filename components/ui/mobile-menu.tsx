cc9fec2257a6754dd8bc2ccdad0cfb9b4323e2fe
vjfrontend <victorezeabikwa@gmail.com>
2025-04-25 03:32:29 -0700

    added gallery page to mobile nav

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -132,7 +132,7 @@ export default function MobileMenu() {
             </li>
             <li>
               <Link
-                href="#"
+                href="/gallery"
                 className="flex rounded-lg px-2 py-1.5 text-gray-700 hover:bg-[#030442]"
                 onClick={() => setMobileNavOpen(false)}
               >
