48ecb304b0de89100e0173159d9c36ca60333f3c
victor ezeabikwa <ifeanyiezeabikwa4@gmail.com>
2025-09-19 14:35:21 +0100

    little fix

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -10,6 +10,7 @@ import Focus from '@/components/focus'
 import Impact from '@/components/impact'
 import Testimonail from '@/components/testimonials-carousel'
 import Sponsors from "@/components/sponsors"
+import Clients from '@/components/clients'
 export default function Home() {
   return (
     <>
@@ -18,6 +19,7 @@ export default function Home() {
       <About/>
       <Impact/>
       <Testimonail/>
+      <Clients/>
       <Sponsors />
            
       
